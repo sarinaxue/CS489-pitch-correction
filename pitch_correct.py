71a8256317e6b48a51e8dfa8d6383af5ca300af4
Lisa Wang <lisa.wang@uwaterloo.ca>
2021-04-08 21:09:10 -0700

    add window/hop size and prevent audio cut off at beginning

--- pitch_correct.py
+++ pitch_correct.py
@@ -24,13 +24,15 @@ img = librosa.display.specshow(D, x_axis='time', y_axis='log', ax=ax)
 ax.set(title='['+sys.argv[1]+'] Original vs Pitch Corrected')
 fig.colorbar(img, ax=ax, format="%+2.f dB")
 ax.plot(times, f0, label='f0', color='cyan', linewidth=3)
+f_target[f_target < 1.03] = np.nan # removes the ftarget rise/drop at beginning/end
 ax.plot(times, f_target, label='ftarget', color='blue', linewidth=1)
 ax.legend(loc='upper right')
 plt.show()
 
 # use PSOLA to move to desired fundamental frequency
 signal_length = signal.shape[-1]
-signal_pitch_corrected = tsm.tdpsola(signal, fs, f0, f_target)
+f_target[np.isnan(f_target)] = 0 # prevents audio from getting cut off
+signal_pitch_corrected = tsm.tdpsola(signal, fs, f0, tgt_f0=f_target, p_hop_size=512, p_win_size=1024)
 sf.write('./wav/'+sys.argv[1]+'_pitch_corrected.wav', signal_pitch_corrected, fs)
 
 
